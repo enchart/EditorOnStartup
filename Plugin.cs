6f5501efbaa9342320ed96e7e08ed96bfd6f5218
enchart <77974051+enchart@users.noreply.github.com>
2024-03-03 16:09:43 +0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using BepInEx;
+
+namespace EditorOnStartup;
+
+[BepInPlugin("com.enchart.editoronstartup", "EditorOnStartup", "1.0.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("Plugin EditorOnStartup is loaded!");
+        StartCoroutine(LoadEditorScene());
+    }
+
+    private static IEnumerator LoadEditorScene()
+    {
+        while (DataManager.inst == null || InputDataManager.inst == null)
+            yield return null;
+        DataManager.inst.UpdateSettingBool("HasPlayedIntro", true);
+        DataManager.inst.UpdateSettingBool("CanEdit", true);
+        InputDataManager.inst.players.Clear();
+        InputDataManager.inst.players.Add(new InputDataManager.CustomPlayer(true, 0));
+        UnityEngine.SceneManagement.SceneManager.LoadScene("editor");
+    }
+}
